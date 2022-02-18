4a604f47920ae7807ba384745f5216a133188d56
Ahmet Cemal Sert <55139481+acmer06@users.noreply.github.com>
2022-02-18 14:22:33 +0300

    firstcommitACS

--- models/News.js
+++ models/News.js
@@ -5,11 +5,19 @@ const NewsSchema = mongoose.Schema({
         type:String,
         required:true
     },
+    title:{
+        type:String,
+        required:true
+    },
     content:{
         type:String,
         required:true
     },
     img:{
+        type:String,
+        required:true,
+    },
+    author:{
         type:String,
         required:true,
     },
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const helmet =  require('helmet');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const dontenv = require('dotenv').config();
 const app = express();
+
+const newsRouter  = require('./routes/news')
 
 //Connect To The DB
 mongoose.connect(process.env.MONGO_URL)
@@ -15,13 +18,25 @@ mongoose.connect(process.env.MONGO_URL)
     console.log("an error happened");
 });
 
-app.use(morgan("dev"));
-app.use(helmet());
+
+const corsOptions = {
+    origin:"http://localhost:3000",
+    optionSuccessStatus:200
+}
 
 app.get('/',(req,res)=>{
     res.send({msg:'Hello From The Server'});
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server is listening on port 5000");
+
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("dev"))
+app.use('/api/news',newsRouter)
+let port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log('Server is runnig on port '+ port +'...')
 })
