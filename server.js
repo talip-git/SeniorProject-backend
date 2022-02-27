67b0cddbde894f15f4b473303fbec30bad20dda6
Sina Postacı <sina.postaci.tedu@gmail.com>
2022-02-27 15:06:10 +0300

    a

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const helmet =  require('helmet');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-const forumCommentsRouter = require('./routes/forumComments')
-
 const dontenv = require('dotenv').config();
+
+//Routes
+const forumCommentsRouter = require('./routes/forumComments')
+//App
 const app = express();
 
 //Connect To The DB
@@ -17,14 +18,12 @@ mongoose.connect(process.env.MONGO_URL)
     console.log("an error happened");
 });
 
+//Middleware
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
 app.use('/api/comments',forumCommentsRouter);
 
-app.get('/',(req,res)=>{
-    res.send({msg:'Hello From The Server'});
-})
 
 app.listen(process.env.PORT,()=>{
     console.log("Server is listening on port 5000");
